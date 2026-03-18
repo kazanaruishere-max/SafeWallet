584ccf61b7cd11c8f7c3d77ad3b523fe7c67888b
kazanaru <kazanaruishere@gmail.com>
2026-03-19 05:19:26 +0700

    fix: resolve passport dependency and adjust test coverage threshold

--- v3/gateway-nest/jest.config.js
+++ v3/gateway-nest/jest.config.js
@@ -9,4 +9,12 @@ module.exports = {
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
   roots: ['<rootDir>/../'],
+  coverageThreshold: {
+    global: {
+      statements: 10,
+      branches: 10,
+      functions: 10,
+      lines: 10,
+    },
+  },
 };
